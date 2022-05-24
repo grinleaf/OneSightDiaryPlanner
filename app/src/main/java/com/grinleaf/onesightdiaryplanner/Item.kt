deb8968a65e940f490c0365546e0161cf56db789
grinleaf <grinleaf774@gmail.com>
2022-05-24 22:25:45 +0900

    포폴 전 점검

--- app/src/main/java/com/grinleaf/onesightdiaryplanner/Item.kt
+++ app/src/main/java/com/grinleaf/onesightdiaryplanner/Item.kt
@@ -59,12 +59,3 @@ data class CategoryImage(var no:String, var image:String)
 data class EmoImage(var no:String, var image:String)
 
 data class SelectedDayEmo(var no:String, var email:String,var day:String, var emo:String)
-
-//공통 데이터 클래스(Base)상속
-//0. base       : 일정 날짜, 일정 내용, 카테고리 이미지
-//1. TimeLine   : ...
-//2. daily      : 타이틀/내용/카테고리이미지/첨부이미지/세부내용
-//3. checklist  : 일정 날짜, 일정 내용, 카테고리 이미지 + 일정 수행여부, 하위항목내용, 하위항목 수행여부
-//4. lifecycle  : 일정 날짜, 일정 내용, 카테고리 이미지 + 반복주기
-//5. Bucketlist : 일정 날짜, 일정 내용, 카테고리 이미지
-//6. Userinfo   : 프로필 이미지, 계정 ID, PW, 닉네임, 등급 점수, ...
